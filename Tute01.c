1feed664ce5d8034a986672d52cf8e4fac336204
HVMHPerera <100770593+HVMHPerera@users.noreply.github.com>
2022-03-02 16:54:38 +0530

    Tute 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+   int mark1,mark2;
+   printf("Enter the Mark 1: ");
+   scanf("%d",&mark1);
+   printf("Enter the Mark 2: ");
+   scanf("%d",&mark2);
+   
+   average=(mark1+mark2)/2;
+      
+   printf("Average is "%d"",average);
+   
   
   return 0;
 }
